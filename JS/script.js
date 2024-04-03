8d597164d0cb196ffedb40c5ac307c7f39bbd206
onniluo <onni.luova@gmail.com>
2024-04-03 13:16:34 +0300

    About sivu päivitetty kuvilla ja menu sivu lisätty.

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,3 @@
-// Simple example for smooth scrolling
 const buttons = document.querySelectorAll('.button');
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
@@ -7,5 +6,21 @@ buttons.forEach(button => {
         const targetSection = document.getElementById(targetId);
 
         targetSection.scrollIntoView({ behavior: 'smooth' });
+    });
+});
+
+const menuButton = document.querySelector('.button[href="menu.html"]'); // Select the specific button
+menuButton.addEventListener('click', function(event) {
+    event.preventDefault(); // Prevent the default link behavior
+    window.location.href = "menu.html"; // Redirect to the menu page
+});
+
+document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.body.classList.add('fade-out');
+        setTimeout(() => {
+            window.location = link.href;
+        }, 5000); // same duration as the fade-out animation
     });
 });
