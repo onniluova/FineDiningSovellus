c2e1e851316ee1bd76bbad6681846517fd96a5c8
onniluo <onni.luova@gmail.com>
2024-04-19 16:38:47 +0300

    Register frontend osuus done

---
+++ JS/register.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+  let registerForm = document.querySelector('.signup form');
+
+  function registerUser(event) {
+    event.preventDefault();
+    console.log('registerUser called');
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+    let etunimi = document.getElementById('first_name').value;
+    let sukunimi = document.getElementById('last_name').value;
+
+    let newUser = {
+      first_name: etunimi,
+      last_name: sukunimi,
+      email: email,
+      password: password,
+    };
+
+    console.log(newUser);
+
+    fetch('http://127.0.0.1:3001/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUser),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Success:', data);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+  registerForm.addEventListener('submit', registerUser);
+});
